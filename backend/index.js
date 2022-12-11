d0e826d13a17c258115acaa72ba44265e497a3ff
Lennardj <lennardvincentjohn@gmail.com>
2022-12-12 12:20:54 +1300

    first commit

---
+++ backend/index.js
@@ -0,0 +1,27 @@
+// index.js
+const express = require("express");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const Routes = require("./routes");
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+
+app.use("/api/agent", Routes);
+
+app.listen(PORT, () => console.log(`🔥  server running on port ${PORT}`));
+
+app.post("/text-input", (req, res) => {
+  res.status(200).send({ data: "TEXT ENDPOINT CONNECTION SUCCESSFUL" });
+});
+
+app.post("/voice-input", (req, res) => {
+  res.status(200).send({ data: "VOICE ENDPOINT CONNECTION SUCCESSFUL" });
+});
+
+module.exports = app;
